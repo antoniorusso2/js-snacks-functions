533269a696281167805cac20440aba7955d5c663
antoniorusso2 <russoantonio303@outlook.it>
2024-10-21 16:05:04 +0200

    ADD: 1st solution for the 1st snack

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -5,9 +5,23 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function firstLetterArray(array){
+  let firstLetters = [];
+
+  for (let i = 0; i < array.length; i++){
+
+    let name = array[i]; //return string
+    // console.log(name)
+
+    firstLetters.push(name.charAt(0)); //return stringa con la prima lettera e la pusha nell'array all'interno della funzione;
+    // console.log(firstLetters)
+  }
+  return firstLetters // la funzione ritorna un array con all'interno le lettere pushate dell'array inserito come parametro;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const result = firstLetterArray(names); //ritorna un array dato dalla funzione
+console.log(result);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
